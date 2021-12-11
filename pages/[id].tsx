b12b935a6fb7d03599e36f19227f81039ce82a6c
shindeajinkya <ajitshinde635@gmail.com>
2021-12-11 21:06:06 +0530

    fix: minor

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -177,7 +177,7 @@ const Profile: React.FC<ProfileProps> = ({ transactions: allTransactions }) => {
 										</div>
 									</div>
 								</div>
-								<div className="mt-8 border-b -mx-8" />
+								<div className="mt-8 border-b -mx-8 xs:hidden" />
 
 								{/* Comments*/}
 								<section aria-labelledby="notes-title">
