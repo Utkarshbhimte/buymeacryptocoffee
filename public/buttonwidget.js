55cf83f632b6178bd0d05d2f9a37f8b2c1deaa70
ajinkya9 <ajitshinde635@gmail.com>
2021-12-21 13:16:45 +0530

    fix: button widget ref

--- public/buttonwidget.js
+++ public/buttonwidget.js
@@ -1,15 +1,6 @@
 
 let widgetScript = document.querySelector('script[data-name="crypto-coffee-button"]');
 
-var s = document.getElementsByTagName('script')[0];
-
-const buttonEventScript = document.createElement('script');
-buttonEventScript.defer = true;
-buttonEventScript.src='https://static.cloudflareinsights.com/beacon.min.js';
-buttonEventScript.dataset.cfBeacon = '{"token": "31fafc582c564dca9461cd9a1e3b6f4f"}'
-
-s.parentNode.insertBefore(buttonEventScript, s);
-
 window.cryptoCoffeeWidget = window.cryptoCoffeeWidget || function(
     // text,
     address,
@@ -20,7 +11,7 @@ window.cryptoCoffeeWidget = window.cryptoCoffeeWidget || function(
     outline_color,
     coffee_color
 ) {
-    const widget = '<div class="crypto-coffee-btn-container"><a class="crypto-coffee-btn" ' + `target="_blank" href="http://buymeacryptocoffee.xyz/${address}">` + "<img src='https://www.buymeacryptocoffee.xyz/embedbadge.svg'/>" + "</a></div>"
+    const widget = '<div class="crypto-coffee-btn-container"><a class="crypto-coffee-btn" ' + `target="_blank" href="http://buymeacryptocoffee.xyz/${address}ref=button_widget>` + "<img src='https://www.buymeacryptocoffee.xyz/embedbadge.svg'/>" + "</a></div>"
     return widget;
 }
 
